9f02bd71a449761914388c4ef40fc06b4039e98c
AIDA <liman-collab>
2020-05-01 02:33:59 +0200

    Updates

---
+++ precache-manifest.5bcaf5283f6e2ff4f9072b4a934e34f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3918d60a12852332ced77f91f873724e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7b09c6cba3700ce2cce8",
+    "url": "/monsters-rolodex/static/css/main.5b2f761d.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b09c6cba3700ce2cce8",
+    "url": "/monsters-rolodex/static/js/main.3e128614.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
